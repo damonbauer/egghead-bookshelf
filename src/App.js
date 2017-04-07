4f35d278c1b9f03ee05bed606f0c1eb141d3682a
Damon Bauer <damon.bauer@daveramsey.com>
2017-04-07 06:47:32 -0500

    Adding styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import books from './books.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,12 +7,11 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <div className="Inner">
+            <img src={books} className="App-logo" alt="logo" />
+            <h2>Egghead Bookshelf</h2>
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
