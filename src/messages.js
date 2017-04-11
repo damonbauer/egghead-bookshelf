a2c2a4a68665bc48e96ddcef4edec41f4d00cd81
Damon Bauer <damon.bauer@daveramsey.com>
2017-04-10 20:44:07 -0500

    Add flattenMessages util, start adding translations and FormattedMessage components

--- src/messages.js
+++ src/messages.js
@@ -1,5 +1,23 @@
 export default {
-  'en-US': {},
-  'es-ES': {},
-  'fr-FR': {}
+  'en-US': {
+    detail: {
+      toggle: 'Toggle',
+      purchase: 'Purchase this book from:',
+      reviewsHeading: 'Reviews'
+    }
+  },
+  'es-ES': {
+    detail: {
+      toggle: 'Palanca',
+      purchase: 'Compre este libro de:',
+      reviewsHeading: 'Comentarios'
+    }
+  },
+  'fr-FR': {
+    detail: {
+      toggle:'Basculer',
+      purchase: 'Achetez ce livre à partir de:',
+      reviewsHeading: 'Avis'
+    }
+  }
 }
