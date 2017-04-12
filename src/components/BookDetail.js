55b2846146dff7c6c569ed8703181cf9f69c742b
Damon Bauer <damon.bauer@daveramsey.com>
2017-04-12 06:21:21 -0500

    Add FormattedMessage components with placeholders

--- src/components/BookDetail.js
+++ src/components/BookDetail.js
@@ -7,6 +7,7 @@ import books from '../books.json';
 const BookDetail = ({match}) => {
   const book = books.find(book => book.id === parseInt(match.params.bookId, 10));
   const sortedReviews = sortBy(book.reviews, 'date').reverse();
+  const avgRating = round(meanBy(book.reviews, (r) => r.rating), 2);
 
   return (
     <div className="BookDetail">
@@ -14,7 +15,9 @@ const BookDetail = ({match}) => {
         <img src={book.image} width="200" height="275" alt={book.title}/>
         <div className="BookDetail-metaBody">
           <h1>{book.title}</h1>
-          <h3>by {book.author}</h3>
+          <h3>
+              <FormattedMessage id="detail.author" values={{author: book.author}} />
+          </h3>
           <div>
             <input type="checkbox" id="toggle" hidden/>
             <p>{book.description}</p>
@@ -41,14 +44,16 @@ const BookDetail = ({match}) => {
       <h2>
         <FormattedMessage id="detail.reviewsHeading"/>
       </h2>
-      <h3>Average Rating: {round(meanBy(book.reviews, (r) => r.rating), 2)} ({book.reviews.length} Reviews)</h3>
+      <h3>
+          <FormattedMessage id="detail.averageRating" values={{ avg: avgRating }} /> ({book.reviews.length} Reviews)
+      </h3>
       <div className="BookDetail-reviews">
         {sortedReviews.map((review) => (
           <div className="Review" key={review.date}>
             <div className="Review-meta">
               <img src={review.avatar} alt="Avatar"/>
               <p>
-                <strong>{review.name}</strong> rated it: {review.rating} out of 5 <br />
+                <FormattedMessage id="detail.userRating" values={{ name: <strong>{review.name}</strong>, rating: review.rating }} /><br />
                 {new Date(review.date).toLocaleDateString()}
               </p>
             </div>
