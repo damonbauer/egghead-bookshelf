42bf25156e34e00c7cb0def623d9dd8bbe591efb
Damon Bauer <hello@damonbauer.me>
2017-04-24 07:16:08 -0500

    Add injectIntl HOC, format textarea placeholder, pull locale from intl prop (#10)

--- src/messages.js
+++ src/messages.js
@@ -7,7 +7,8 @@ export default {
       window: '<small><em>All {numMerchants} links open in a new window.</em></small>',
       reviewsHeading: 'Reviews',
       averageRating: 'Average Rating: {avg} ({count, plural, =0 {No Reviews Yet!} one {# Review} other {# Reviews}})',
-      userRating: '{name} rated it: {rating} out of 5'
+      userRating: '{name} rated it: {rating} out of 5',
+      inputPlaceholder: 'What did you think? Enter your review here.'
     }
   },
   'es-ES': {
@@ -18,7 +19,8 @@ export default {
       window: '<small><em>Los {numMerchants} enlaces se abren en una nueva ventana.</em></small>',
       reviewsHeading: 'Comentarios',
       averageRating: 'Puntuación media: {avg} ({count, plural, =0 {No hay comentarios todavía!} one {# Crítica} other {# Críticas}})',
-      userRating: '{name} clasificado: {rating} de 5'
+      userRating: '{name} clasificado: {rating} de 5',
+      inputPlaceholder: '¿Que piensas? Introduzca su comentario aquí.'
     }
   },
   'fr-FR': {
@@ -29,7 +31,8 @@ export default {
       window: '<small><em>Les {numMerchants} liens s\'ouvrent dans une nouvelle fenêtre.</em></small>',
       reviewsHeading: 'Avis',
       averageRating: 'Note moyenne: {avg} ({count, plural, =0 {Pas encore de commentaires!} one {# La revue} other {# Avis}})',
-      userRating: '{name} L\'a noté: {rating} sur 5'
+      userRating: '{name} L\'a noté: {rating} sur 5',
+      inputPlaceholder: 'Qu\'as-tu pensé? Entrez votre avis ici.'
     }
   }
 }
