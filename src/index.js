1cf2dfaba7ba840ce1a3276721169865978e7e81
Damon Bauer <damon.bauer@daveramsey.com>
2017-04-25 06:21:24 -0500

    Add intl polyfill & require scripts

--- src/index.js
+++ src/index.js
@@ -12,6 +12,20 @@ import messages from './messages';
 import App from './App';
 import './index.css';
 
+if (!window.intl) {
+  require.ensure([
+    'intl',
+    'intl/locale-data/jsonp/en.js',
+    'intl/locale-data/jsonp/fr.js',
+    'intl/locale-data/jsonp/es.js',
+  ], (require) => {
+    require('intl');
+    require('intl/locale-data/jsonp/en.js');
+    require('intl/locale-data/jsonp/fr.js');
+    require('intl/locale-data/jsonp/es.js');
+  })
+}
+
 addLocaleData([...en, ...fr, ...es]);
 
 let locale = (navigator.languages && navigator.languages[0])
