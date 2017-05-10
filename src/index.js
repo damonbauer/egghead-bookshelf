ba4f13c3c34a6bc5c0724db53bff70f65b52a305
Damon Bauer <damonbauer@users.noreply.github.com>
2017-05-10 06:07:59 -0500

    Lesson11 - Conditionally require `intl` polyfill (#13)
    * Add intl polyfill & require scripts
    * Add wrapper method, update window.Intl check

--- src/index.js
+++ src/index.js
@@ -12,16 +12,36 @@ import messages from './messages';
 import App from './App';
 import './index.css';
 
-addLocaleData([...en, ...fr, ...es]);
+if (!window.Intl) {
+  require.ensure([
+    'intl',
+    'intl/locale-data/jsonp/en.js',
+    'intl/locale-data/jsonp/fr.js',
+    'intl/locale-data/jsonp/es.js'
+  ], (require) => {
+    require('intl');
+    require('intl/locale-data/jsonp/en.js');
+    require('intl/locale-data/jsonp/fr.js');
+    require('intl/locale-data/jsonp/es.js');
 
-let locale = (navigator.languages && navigator.languages[0])
-             || navigator.language
-             || navigator.userLanguage
-             || 'en-US';
+    runApp();
+  })
+} else {
+  runApp();
+}
 
-ReactDOM.render(
-  <IntlProvider locale={locale} messages={flattenMessages(messages[locale])}>
-    <App />
-  </IntlProvider>,
-  document.getElementById('root')
-);
+function runApp() {
+  addLocaleData([...en, ...fr, ...es]);
+
+  let locale = (navigator.languages && navigator.languages[0])
+    || navigator.language
+    || navigator.userLanguage
+    || 'en-US';
+
+  ReactDOM.render(
+    <IntlProvider locale={locale} messages={flattenMessages(messages[locale])}>
+      <App />
+    </IntlProvider>,
+    document.getElementById('root')
+  );
+}
